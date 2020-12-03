c3979e31ccabd81e94200335c0bd86b72591e4d6
morevi <1998morevi@gmail.com>
2020-12-03 15:10:41 +0100

    Fixed copy of youtube-dl config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-apt update && apt upgrade
+
 termux-setup-storage
+
+apt update && apt upgrade
 pkg install python
 pkg install ffmpeg
 pip install youtube-dl
@@ -9,5 +11,5 @@ mkdir /data/data/com.termux/files/home/storage/shared/Youtube
 mkdir ~/bin
 cp termux-url-opener ~/bin
 
-mkdir .config/youtube-dl/
-cp ytdl.config .config/youtube-dl
+mkdir -p ~/.config/youtube-dl/
+cp ytdl.config ~/.config/youtube-dl/config
